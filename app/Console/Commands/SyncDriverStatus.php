0d3df2b2fa0ed2fbe4e46b076fbdc9fe9bb68b47
Peter Hartawan <peter.s@tokopedia.com>
2021-08-16 01:29:32 +0700

    - Add log sync driver

--- app/Console/Commands/SyncDriverStatus.php
+++ app/Console/Commands/SyncDriverStatus.php
@@ -10,6 +10,7 @@ use App\Services\UtilitiesServices;
 use DB;
 use App\Model\OperOrder;
 use App\Model\BookingInfo;
+use Illuminate\Support\Facades\Log;
 
 class SyncDriverStatus extends Command
 {
@@ -51,73 +52,79 @@ class SyncDriverStatus extends Command
 
         $token = new UtilitiesServices();
         $token = $token->getRecentOpertaskToken();
-        
+
 
         /**
          * Get every single booking info that has oper_orders with status 0, or 6
          */
 
         $booking_order_info = DB::table('booking_order_info AS A')
-                                ->leftJoin('oper_orders AS B', 'A.booking_no', '=', 'B.booking_no')
-                                ->whereIn('B.order_status', [
-                                    OperOrder::WAITING_FOR_DRIVER,
-                                    OperOrder::WAITING_FOR_DRIVER_AFTER_INVOICE
-                                ])
-                                ->select('A.oper_task_order_id', 'B.id AS order_id', 'A.order_state')
-                                ->get();
+            ->leftJoin('oper_orders AS B', 'A.booking_no', '=', 'B.booking_no')
+            ->whereIn('B.order_status', [
+                OperOrder::WAITING_FOR_DRIVER,
+                OperOrder::WAITING_FOR_DRIVER_AFTER_INVOICE
+            ])
+            ->select('A.oper_task_order_id', 'B.id AS order_id', 'A.order_state')
+            ->get();
+
+        Log::info('SYNC_DRIVER:BOOKING_ORDER_INFO', [$booking_order_info]);
 
         $service = new OperTaskServices();
 
         /**
          * Loop through booking_order_info
          */
-        foreach($booking_order_info as $set){
+        $totalChangedData = 0;
+        foreach ($booking_order_info as $set) {
 
             $order = OperOrder
-                        ::where('id', $set->order_id)
-                        ->get()
-                        ->first();
+                ::where('id', $set->order_id)
+                ->get()
+                ->first();
 
             /**
-             * Web hook 
+             * Web hook
              */
             $response = $service->getOrderByIdOrder(
                 $set->oper_task_order_id,
                 $token
             );
+            Log::info('SYNC_DRIVER:GET_ORDER_BY_ID_ORDER', [$booking_order_info]);
 
             /**
              * If the driver is not exist, skip this loop.
              */
 
-            if($response->data->driver === null){
+            if ($response->data->driver === null) {
                 continue;
             }
-            
+
             /**
              * Proceed update status
              */
-            switch($set->order_state){
-
-                case BookingInfo::PICKUP_STATE_ORDER: 
+            switch ($set->order_state) {
+                case BookingInfo::PICKUP_STATE_ORDER:
 
                     $order->order_status = 1;
                     $order->save();
+                    $totalChangedData++;
 
-                        break;
+                    break;
 
-                case BookingInfo::DELIVERY_STATE_ORDER: 
-                        
+                case BookingInfo::DELIVERY_STATE_ORDER:
+
                     $order->order_status = 7;
                     $order->save();
+                    $totalChangedData++;
 
-                        break;
+                    break;
 
-                default: 
+                default:
             }
 
         }
 
-        return 0;
+        Log::info('SYNC_DRIVER:TOTAL_CHANGED_DATA', [$totalChangedData]);
+        return $totalChangedData;
     }
 }
